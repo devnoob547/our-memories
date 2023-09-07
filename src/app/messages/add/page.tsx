1364a69fc436ac7d80cc67a6e1df012dc98e7020
devnoob547 <danielalvespessoa3006@gmail.com>
2023-09-07 16:06:46 -0300

    end form

--- src/app/messages/add/page.tsx
+++ src/app/messages/add/page.tsx
@@ -1,4 +1,10 @@
+'use client'
+import { FormEvent, useState } from 'react'
+import { useForm } from 'react-hook-form'
+
 export default function AddMessage() {
+  const { register, handleSubmit } = useForm()
+
   return (
     <div className="flex min-h-screen min-w-full items-center justify-center flex-col space-y-6">
       <div className="flex flex-col items-center justify-center space-y-12">
@@ -6,10 +12,19 @@ export default function AddMessage() {
         <h2 className="font-light">Enviar mensagem</h2>
       </div>
       <div className="mx-8 bg-zinc-700 p-6 rounded-md shadow-md shadow-zinc-900">
-        <form action="" className="flex items-center justify-center flex-col gap-4">
-          <input type="text" placeholder="Nome" className="bg-zinc-900 p-4 rounded-md text-sm border border-zinc-600 focus:border-zinc-400 transition-colors duration-300 outline-none" />
-          <textarea placeholder="Mensagem" rows={5} className="bg-zinc-900 p-4 rounded-md text-sm border border-zinc-600 resize-none w-full focus:border-zinc-400 transition-colors duration-300 outline-none" />
-          <button type="submit" className="w-full bg-green-500 text-white p-2 rounded-md hover:bg-green-600 transition-all duration-300 hover:shadow-md hover:shadow-zinc-900">Enviar</button>
+        <form 
+          action=""
+          className="flex items-center justify-center flex-col gap-4"
+          onSubmit={handleSubmit((data) => console.log(data))}
+        >
+          <input type="text" {...register('name')} placeholder="Nome" className="bg-zinc-900 p-4 rounded-md text-sm border border-zinc-600 focus:border-zinc-400 transition-colors duration-300 outline-none" />
+          <textarea placeholder="Mensagem" {...register('message')} rows={5} className="bg-zinc-900 p-4 rounded-md text-sm border border-zinc-600 resize-none w-full focus:border-zinc-400 transition-colors duration-300 outline-none" />
+          <button 
+            type="submit"
+            className="w-full bg-green-500 text-white p-2 rounded-md hover:bg-green-600 transition-all duration-300 hover:shadow-md hover:shadow-zinc-900"
+          >
+            Enviar
+          </button>
         </form>
       </div>
     </div>
